66e8e89b7def383bf2c2540f0c765efa81bc7a96
ElyorxonB <117801522+ElyorxonB@users.noreply.github.com>
2022-12-25 08:10:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var str = +prompt('Введите число №1: ');
+
+console.log((+str))
+
+var str2 = +prompt('Введите число №2: ');
+
+console.log((+str2))
+
+var str3 = +prompt('Введите число №3: ');
+
+console.log((+str3))
+
+console.log((+str + +str2 + +str3)/3)
+
+
+alert ((+str + +str2 + +str3)/3)
